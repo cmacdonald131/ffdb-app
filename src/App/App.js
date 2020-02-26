e7c3edece180d9f6dc226dce0f3fe39c3191314d
Christopher MacDonald <cmacdonald131@gmail.com>
2020-02-26 15:22:38 -0500

    commit

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,12 @@ import { Route } from 'react-router-dom';
 import About from '../About/About';
 import TeamPage from '../TeamPage/TeamPage';
 import AddTeam from '../AddTeam/AddTeam';
+import PrivateRoute from '../Utils/PrivateRoute'
+import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import LoginForm from '../LoginForm/LoginForm'
+import RegistrationForm from '../RegistrationForm/RegistrationForm'
+//import ArticlePage from '../Routes/ArticlePage/ArticlePage'
+
 
 import './App.css';
 
@@ -14,6 +20,18 @@ class App extends Component {
         <Route exact path="/" component={About} />
         <Route exact path="/team-page" component={TeamPage} />
         <Route exact path="/team-page/add-team" component={AddTeam} />
+        <PublicOnlyRoute
+          path={'/login'}
+          component={LoginForm}
+        />
+        <PublicOnlyRoute
+          path={'/register'}
+          component={RegistrationForm}
+        />
+        <PrivateRoute
+              path={'/article/:articleId'}
+              //component={ArticlePage}
+            />
       </div>
     );
   }
