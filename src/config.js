957ed6fcf1098d6683184a686fca0e29dc8d08e9
Christopher MacDonald <cmacdonald131@gmail.com>
2020-03-03 20:51:39 -0500

    fix endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT ||'http://localhost:8000/api', //'https://nameless-journey-62926.herokuapp.com/api',
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT ||//'http://localhost:8000/api', 
+  'https://nameless-journey-62926.herokuapp.com/api',
   API_TOKEN: process.env.REACT_APP_API_TOKEN || "ffdb"
 }
